52ad29b684d7ad578f49d589f10c07fad7984c2d
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-06-27 17:19:58 -0700

    Add dropdown to header

--- ecommerce-react-site/src/pages/components/Header.js
+++ ecommerce-react-site/src/pages/components/Header.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
 import HeaderCSS from './Header.module.css'
 import { BiSolidCart } from "react-icons/bi";
 import { GiHamburgerMenu } from "react-icons/gi"
 
+const DropDown = ({toggle}) => {
+    if (!toggle) {
+        return null;
+    }
+    return (
+        <div className={HeaderCSS.dropDown}>
+            <ul>
+                <li>New</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+        </div>
+    );
+}
+
 const Header = () => {
+    let [toggleDropDown, setToggleDropDown] = useState(false);
     return (
         <header>
             <h1 className={HeaderCSS.title}>Name</h1>
@@ -15,8 +33,10 @@ const Header = () => {
                 </ul>
             </nav>
             <div className={HeaderCSS.icons}>
-                <GiHamburgerMenu className={HeaderCSS.menu}/>
-                <BiSolidCart />
+                <GiHamburgerMenu className={HeaderCSS.menu} onClick={() => setToggleDropDown(!toggleDropDown)}/>
+                <BiSolidCart className={HeaderCSS.cart}/>
+
+                <DropDown toggle={toggleDropDown}/>
             </div>
         </header>
     );
