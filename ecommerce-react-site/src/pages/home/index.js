4f28ff1642abe690135812857c57e743b06825ab
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-06-29 20:37:15 -0700

    Get API data

--- ecommerce-react-site/src/pages/home/index.js
+++ ecommerce-react-site/src/pages/home/index.js
@@ -1,6 +1,19 @@
-import Header from "../components/Header";
+import { useState, useEffect, useCallback } from 'react';
+import Header from "../../components/Header";
 
 const Home = () => {
+    let [products, setProducts] = useState([]);
+
+    const fetchData = useCallback(() => {
+        fetch('https://fakestoreapi.com/products')
+          .then(res => res.json())
+          .then(data => setProducts(data))
+    }, []);
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData]);
+
     return (
         <>
             <Header />
