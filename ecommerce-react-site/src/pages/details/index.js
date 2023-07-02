984bc028c2153e2a4f397490a6eed7d84c59dc52
Benjamin Pham <97713069+BenPham14@users.noreply.github.com>
2023-07-02 16:17:56 -0700

    Fix navigation and css for Details page

--- ecommerce-react-site/src/pages/details/index.js
+++ ecommerce-react-site/src/pages/details/index.js
@@ -1,13 +1,16 @@
 import { useState, useCallback, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import DetailsCSS from "../../styles/Details.module.css";
 import Header from "../../components/Header";
-import Loading from "../../components/Loading";
+import Loading from "../../components/Loading"; 
+import { VscChevronDown, VscChevronUp } from 'react-icons/vsc';
 
 const Details = ({page}) => {
     let [products, setProducts] = useState([]);
     let [loading, setLoading] = useState(false);
+    let [description, setDescription] = useState(true);
     const params = useParams();
+    const navigate = useNavigate();
 
     const fetchData = useCallback(() => {
         setLoading(true);
@@ -32,16 +35,37 @@ const Details = ({page}) => {
             <Header />
             <main>
                 <div className={DetailsCSS.nav}>
-                    <button>{page}</button>
+                    <Link 
+                        to={products.category == "men's clothing" ? '/men' : (products.category == "women's clothing" ? "/women" : "/other")}
+                        style={{color: 'black'}}
+                    >
+                        {products.category == "men's clothing" ? 'Men' : (products.category == "women's clothing" ? "Women" : "Other")}
+                    </Link>
                     <p>/</p>
                     <p>Details</p>
                 </div>
                 <div className={DetailsCSS.product}>
                     <img src={products.image} alt=""/>
-                    <div className={DetailsCSS.description}>
-                        <p>{products.title}</p>
-                        <p>${products.price}</p>
-                        <p></p>
+                    <div>
+                        <h2>{products.title}</h2>
+                            <h3>${products.price}</h3>
+                            {/* <div>
+                                <p>stars</p>
+                                <p>{products.rating.rate}</p>
+                            </div> */}
+                        <hr/>
+                        <div className={DetailsCSS.description}>
+                            <div>
+                                <h3>Description</h3>
+                                {
+                                    description ? 
+                                        <VscChevronUp style={{width: '20px', height:'auto'}} onClick={() => setDescription(!description)} className={DetailsCSS.toggle}/> :
+                                        <VscChevronDown style={{width: '20px', height:'auto'}} onClick={() => setDescription(!description)} className={DetailsCSS.toggle}/>
+                                }
+                             </div>
+                            <p>{description && products.description}</p>
+                        </div>
+                        
                     </div>
                 </div>
             </main>
